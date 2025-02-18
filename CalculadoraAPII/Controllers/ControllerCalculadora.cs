0d03c21422b320d534a1a1b3065de5f4dffcbc23
Jorge-coder-png <a2023211760@estudiantes.upsa.edu.bo>
2025-02-17 21:28:43 -0400

    Probandi comentarios

--- CalculadoraAPII/Controllers/ControllerCalculadora.cs
+++ CalculadoraAPII/Controllers/ControllerCalculadora.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace CalculadoraAPII.Controllers;
-
-public class ControllerCalculadora : Controller
+[ApiController]
+[Route("[controller]")]
+public class ControllerCalculadora : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    [HttpGet("operar")]
+    public IActionResult Operar(string operacion, [FromQuery] int a, [FromQuery] int b)
     {
-        return View();
+        double resultadoOperacion;
+
+        switch (operacion.ToLower())
+        {
+            case "sumar":
+                resultadoOperacion = a + b;
+                break;
+            case "restar":
+                resultadoOperacion = a - b;
+                break;
+            case "multiplicar":
+                resultadoOperacion = a * b;
+                break;
+            case "dividir":
+                if (b == 0)
+                {
+                    return BadRequest(new { error = "No se puede dividir por cero." });
+                }
+                resultadoOperacion = (double)a / b;
+                break;
+            default:
+                return BadRequest(new { error = "Operación inválida." });
+        }
+
+        var resultado = new
+        {
+            a,
+            b,
+            operacion,
+            resultadoOperacion
+        };
+
+        return Ok(resultado);
     }
 }
